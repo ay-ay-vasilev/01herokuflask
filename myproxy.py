3e825f744b3e16af849d98116ebd42813f8c7c1c
aytal.vasilyev <aytal.vasilyev@mytona.com>
2021-04-08 18:47:20 +0900

    test

--- myproxy.py
+++ myproxy.py
@@ -10,35 +10,35 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    msg = ""
-    if request.args.get('url'):
-        try:
-            r = requests.get(request.args['url'])
-            if r.status_code == 200:
-                ip_client = request.remote_addr
-                J = json.load(open('/config/config.json'))
-	            ip = J['ip']
-                now = datetime.now()
-                cnx = mysql.connector.connect(user='myproxy', database='myproxy', host=ip, password='1234zxcv')
-                cursor = cnx.cursor()
-                cursor.execute(
-                    "INSERT INTO myproxy.log VALUES(%s, %s, %s)",
-                    (now, ip_client, request.args['url'])
-                )
-                cnx.commit()
-                return  r.content
+	msg = ""
+	if request.args.get('url'):
+		try:
+			r = requests.get(request.args['url'])
+			if r.status_code == 200:
+				ip_client = request.remote_addr
+				J = json.load(open('/config/config.json'))
+				ip = J['ip']
+				now = datetime.now()
+				cnx = mysql.connector.connect(user='myproxy', database='myproxy', host=ip, password='1234zxcv')
+				cursor = cnx.cursor()
+				cursor.execute(
+					"INSERT INTO myproxy.log VALUES(%s, %s, %s)",
+					(now, ip_client, request.args['url'])
+				)
+				cnx.commit()
+				return  r.content
 
-        except:
-            msg = f"Не удалось загрузить {request.args.get('url')}"
-    return  '<h1>' + msg + '''</h1>
-            Hey IVT-17!<br/>
-            <form method="get">
-                <input type="text" name="url">
-                <input type="submit">
-            </form>
-            '''
-    
+		except:
+			msg = f"Не удалось загрузить {request.args.get('url')}"
+	return  '<h1>' + msg + '''</h1>
+			Hey IVT-17!<br/>
+			<form method="get">
+				<input type="text" name="url">
+				<input type="submit">
+			</form>
+			'''
+	
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
