7a4ab4622da57e2ff250ae8519c4176cf92cc474
aytal.vasilyev <aytal.vasilyev@mytona.com>
2021-04-08 18:36:55 +0900

    log test commit

--- myproxy.py
+++ myproxy.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import request
 import requests
 
+import mysql.connector
+from datetime import datetime
+import json
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -11,7 +15,21 @@ def hello_world():
         try:
             r = requests.get(request.args['url'])
             if r.status_code == 200:
+                
+                ip_client = request.remote_addr
+                J = json.load(open('/config/config.json'))
+	            ip = J['ip']
+
+                now = datetime.now()
+                cnx = mysql.connector.connect(user='myproxy', database='myproxy', host=ip_client, password='1234zxcv')
+                cursor = cnx.cursor()
+                cursor.execute(
+                    "INSERT INTO myproxy.log VALUES(%s, %s, %s)",
+                    (now, ip, "http://b.com")
+                )
+                cnx.commit()
                 return  r.content
+
         except:
             msg = f"Не удалось загрузить {request.args.get('url')}"
     return  '<h1>' + msg + '''</h1>
@@ -21,6 +39,8 @@ def hello_world():
                 <input type="submit">
             </form>
             '''
+    
+
 
 if __name__ == '__main__':
     app.run()
